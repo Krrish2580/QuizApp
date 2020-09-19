37099a16e0761a08e556439597ea4cde11ec8594
Krrish2580 <krrishgaihre@gmail.com>
2020-09-19 19:31:41 +0545

    Quiz app completed

---
+++ question.js
@@ -0,0 +1,75 @@
+//Array of Objects
+const quiz = [
+  {
+    q:
+      "In which decade was the American Institute of Electrical Engineers (AIEE) founded?",
+    options: ["1850s", "1880s", "1930s", "1950s"],
+    answer: 1,
+  },
+  {
+    q: "What part of a database that holds only one type of information?",
+    options: ["Report", "Field", "Record", "File"],
+    answer: 1,
+  },
+  {
+    q:
+      "Which is a type of Electrically-Erasable Programmable Read-Only Memory?",
+    options: ["Flash", "Flange", "Fury", "FRAM"],
+    answer: 0,
+  },
+  {
+    q: "The purpose of choke in tube light is?",
+    options: [
+      "To decrease the current",
+      "To increase the current",
+      "To decrease the voltage momentarily",
+      "To increase the voltage momentarily",
+    ],
+    answer: 3,
+  },
+  {
+    q: "Who developed Yahoo?",
+    options: [
+      "Dennis Ritchie & Ken Thompson",
+      "David Filo & Jerry Yang",
+      "Vint Cerf & Robert Kahn",
+      "Steve Case & Jeff Bezos",
+    ],
+    answer: 1,
+  },
+  {
+    q: "What does the term PLC stand for?",
+    options: [
+      "Programmable Lift Computer",
+      "Program List Control",
+      "Programmable Logic Controller",
+      "Piezo Lamp Connector",
+    ],
+    answer: 2,
+  },
+  {
+    q: "DB computer abbreviation usually means?",
+    options: ["Database", "Double Byte", "Data Block", "Driver Boot"],
+    answer: 0,
+  },
+  {
+    q: "Which company created the most used networking software in the 1980s",
+    options: ["Microsoft", "Sun", "IBM", "Novell"],
+    answer: 3,
+  },
+  {
+    q: "Which of the following operating systems is produced by IBM?",
+    options: ["OS-2", "Windows", "DOS", "UNIX"],
+    answer: 0,
+  },
+  {
+    q: "What is a NIC?",
+    options: [
+      "Netware Intranet Controller",
+      "No Internet Connection",
+      "Network Interface Card",
+      "Network Interference Control",
+    ],
+    answer: 2,
+  },
+];
